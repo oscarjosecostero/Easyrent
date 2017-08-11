c7b33d8ba136354975a8f22cbdda96ea88606945
Oscar Costero <oscarjosecostero@gmail.com>
2017-08-11 18:52:22 -0400

    configure welcome email

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,17 @@
 # Load the Rails application.
 require_relative 'application'
 
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'gbrlmrllo',
+  :password => 'easyrent2017',
+  :domain => 'localhost:3000',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 # Initialize the Rails application.
 Rails.application.initialize!
+
