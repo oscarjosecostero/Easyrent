9a503687cfc6dab900d5312152f66914c3c7c425
Oscar Costero <oscarjosecostero@gmail.com>
2017-08-14 10:51:35 -0400

    review changes

--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,14 @@
 # Load the Rails application.
 require_relative 'application'
 
-
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'gbrlmrllo',
-  :password => 'easyrent2017',
-  :domain => 'localhost:3000',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'localhost:3000',
+  adress: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
 }
-
 # Initialize the Rails application.
 Rails.application.initialize!
-
