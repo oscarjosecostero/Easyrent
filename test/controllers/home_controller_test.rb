49b264e74913bc5bc7c57655c1ff22f47183494b
Oscar Costero <oscarjosecostero@gmail.com>
2017-08-14 18:40:29 -0400

    Review changes and rubocop ofenses solved

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
+# Home Controller Test
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test'should get index' do
     get home_index_url
     assert_response :success
   end
-
 end
